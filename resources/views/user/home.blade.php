323b731d52f67384aca32c3645371b89cf4b9fc8
Iman Maulana <maulanaiman902@gmail.com>
2024-07-19 20:37:21 +0700

    style: fix emoji size in hero section for desktop screen

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -10,9 +10,9 @@
             <h1 class="text-sm uppercase tracking-wide text-neutral-500 lg:text-lg">Welcome to Focus</h1>
             <h2 class="text-xl font-serif font-medium mt-4 md:max-w-xl md:mx-auto lg:max-w-3xl lg:text-3xl">Catch Up on the
                 <span class="text-red-600">Trusted News</span>
-                <span class="text-xl">✅</span>, Stay Ahead with <span class="text-red-600">Accurate</span> <span
-                    class="text-xl">🎯</span> updates, and
-                Your Daily Dose of <span class="text-red-600">Insight</span> <span class="text-xl">✨</span>
+                <span class="text-xl lg:text-3xl">✅</span>, Stay Ahead with <span class="text-red-600">Accurate</span> <span
+                    class="text-xl lg:text-3xl">🎯</span> updates, and
+                Your Daily Dose of <span class="text-red-600">Insight</span> <span class="text-xl lg:text-3xl">✨</span>
             </h2>
         </div>
     </section>
